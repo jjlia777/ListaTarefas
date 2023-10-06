e30134144595e4dbfd561f8b9a89e41efad28764
Júlia Rosa <133364820+jjlia777@users.noreply.github.com>
2023-10-06 16:53:40 -0300

    Add files via upload

--- cadastro/main.js
+++ cadastro/main.js
@@ -8,6 +8,7 @@ const emailInput = form.querySelector("input[type='email']")
 //Seleciona o campo de entrado do email
 const senhaInput = form.querySelector("input[type= 'password']")
 //Seleciona o campo de entrada da senha
+const telInput = form.querySelector("input[type= 'tel']")
 
 //Adiciona um ouvinte de eventos ao formulario para detectarmos o usuario clicar no botão
 form.addEventListener('submit', (event) => {
@@ -18,11 +19,12 @@ form.addEventListener('submit', (event) => {
     const email = emailInput.value.trim() ;
     const usuario = usuarioInput.value.trim() ;
     const senha = senhaInput.value.trim() ; 
+    const telefone = telInput.value.trim();
 
     //Verifica se os campos de usuario e senha estão preenchidos
-    if(usuario === '' || senha === '' || email === '') {
+    if(usuario === '' || senha === '' || email === '' || telefone === '') {
         alert("Por favor, preencha o campos");
-    } else if (usuario === 'usuario' && senha === 'senha' && email === 'email@email.com') {
+    } else if (usuario === 'usuario' && senha === 'senha' && email === 'email@email.com' && telefone === '123456789') {
         //Verifica se as informações de login e senha estão corretas
         alert("Bem vindo!")
         location.replace("/tarefa/index.html", "_self")
